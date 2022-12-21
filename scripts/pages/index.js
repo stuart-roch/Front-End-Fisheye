5797deffb3de48411c2fb1160a9526388588e976
Stuart ROCH <stuart.roch@hotmail.fr>
2022-12-21 13:36:46 +0100

    correction url

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,8 +1,8 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers=[]
-        fetch("../../data/photographers.json")
+        //let photographers=[]
+        fetch("./data/photographers.json")
         .then(function(response){
             return response.json()
         })
