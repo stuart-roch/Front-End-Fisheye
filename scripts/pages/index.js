e966bb9f05a90928172bde546ffcbabe00034cde
Stuart ROCH <stuart.roch@hotmail.fr>
2022-12-21 12:34:00 +0100

    ajout des photos, affichage de l'index avec les données du fichier json

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,7 +1,15 @@
     async function getPhotographers() {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
+        let photographers=[]
+        fetch("../../data/photographers.json")
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(data){
+            displayData(data["photographers"])
+        })
+            /*[
             {
                 "name": "Ma data test",
                 "id": 1,
@@ -20,10 +28,10 @@
                 "price": 500,
                 "portrait": "account.png"
             },
-        ]
+        ]*/
         // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        /*return ({
+            photographers: [...photographers, ...photographers, ...photographers]})*/
     }
 
     async function displayData(photographers) {
@@ -38,8 +46,9 @@
 
     async function init() {
         // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
+        //const { photographers } = 
+        await getPhotographers();
+        //displayData(photographers);
     };
     
     init();
