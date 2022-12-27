48063e5b2b5d2188ed0b0e490a475edab1dc39fc
Stuart ROCH <stuart.roch@hotmail.fr>
2022-12-27 20:05:18 +0100

    intégration page d'accueil

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,4 +1,4 @@
-function photographerFactory(data) {
+/*function photographerFactory(data) {
     const { name, portrait } = data;
 
     const picture = `assets/photographers/Photographers ID Photos/${portrait}`;
@@ -14,4 +14,65 @@ function photographerFactory(data) {
         return (article);
     }
     return { name, picture, getUserCardDOM }
+}*/
+
+class photographerFactory{
+    constructor(data){
+        this.name=data.name;
+        this.id=data.id;
+        this.city=data.city;
+        this.country=data.country;
+        this.tagline=data.tagline;
+        this.price=data.price;
+        this.portrait=data.portrait;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    getCity(){
+        return this.city;
+    }
+
+    getCountry(){
+        return this.country;
+    }
+
+    getTagline(){
+        return this.tagline;
+    }
+    getPrice(){
+        return this.price;
+    }
+    getPortrait(){
+        return `assets/photographers/Photographers ID Photos/`+this.portrait;
+    }
+
+    getUserCardDOM() {
+        const article = document.createElement( 'article' );
+        const link = document.createElement('a');
+        link.setAttribute("href","./photographer.html/");
+        article.appendChild(link);
+        const img = document.createElement( 'img' );
+        img.setAttribute("src", this.getPortrait())
+        const h2 = document.createElement( 'h2' );
+        h2.textContent = this.getName();
+        const location = document.createElement('strong');
+        location.textContent = this.getCity() + ", " + this.getCountry();
+        const paragrapheTagline=document.createElement('p');
+        paragrapheTagline.textContent=this.getTagline();
+        const paragraphePrice=document.createElement('strong');
+        paragraphePrice.textContent=this.getPrice()+"/jour";
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(location)
+        article.appendChild(paragrapheTagline);
+        article.appendChild(paragraphePrice);
+        return (article);
+    }
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -38,7 +38,7 @@
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
+            const photographerModel = new photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
         });
